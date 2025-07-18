80d3a12e339d634f2b618cde875f3a749ba86936
arshiamaremmat <arshiamaremmat27@gmail.com>
2025-07-18 14:32:18 -0700

    Analyzing Data with Modules and Syntax(JS)

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const array of args) {
+    combinedObject.users.push(...array);
+  }
+
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
